fd6e6b4ab2660f291b47918debc3283d0b9ad03c
Captagonal <KOC540916@stu.aasd.k12.wi.us>
2024-11-10 17:04:20 -0600

    Quartz sync: Nov 10, 2024, 5:04 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "captagonal.github.io/Heroica-Rules",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
